f1ac0a2ac3ec3685efc47ec681ef807eeefa1e47
Lavínia Rodrigues <96793999+Laviniarm@users.noreply.github.com>
2025-03-16 23:26:04 -0300

    Paginação concluida

--- src/main/java/br/edu/ifpb/pweb2/retrato/repository/PhotographerRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/retrato/repository/PhotographerRepository.java
@@ -2,6 +2,8 @@ package br.edu.ifpb.pweb2.retrato.repository;
 
 import br.edu.ifpb.pweb2.retrato.model.Photographer;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +19,6 @@ public interface PhotographerRepository extends JpaRepository<Photographer, Inte
     Optional<Photographer> findById(Integer id);
 
     Photographer findByEmail(String email);
+
+    Page<Photographer> findByIdNot(Integer id, Pageable pageable);
 }
